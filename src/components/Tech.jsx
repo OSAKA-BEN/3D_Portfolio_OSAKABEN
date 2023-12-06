16528b8f4c39ee01a2948ccc95520b1b0266710d
benjamin guiganton <benjamin.guiganton@gmail.com>
2023-12-06 09:00:02 +0900

    build: :construction: Composant Tech finalisé en structure et responsive

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,39 +7,47 @@ import { fadeIn } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <motion.div variants={fadeIn("up", "spring", 0, 0.75)}  className='flex flex-col items-center'>
-      <p className='text-description md:text-[30px] text-[15px] p-4'>FRONTEND</p>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologiesFront.map((technology) => (
-          <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
-            <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
-            <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-              <span className='text-white text-sm'>{technology.name}</span>
+    <motion.div variants={fadeIn("up", "spring", 0, 0.75)}  className='flex flex-col sm:flex-row w-full gap-4'>
+      <div className='flex flex-col flex-1'>
+        <p className='text-title md:text-[30px] text-[24px] text-center p-4'>FRONTEND</p>
+        <div className='flex flex-row flex-wrap gap-5 justify-center'>
+          {technologiesFront.map((technology) => (
+            <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
+              <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
+              <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                <span className='text-white text-sm'>{technology.name}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <p className='text-description md:text-[30px] text-[15px] p-4'>BACKEND</p>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologiesBack.map((technology) => (
-          <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
-            <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
-            <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-              <span className='text-white text-sm'>{technology.name}</span>
+
+      <div className='flex flex-col flex-1'>
+        <p className='text-title md:text-[30px] text-[24px] text-center p-4'>BACKEND</p>
+        <div className='flex flex-row flex-wrap gap-5 justify-center'>
+          {technologiesBack.map((technology) => (
+            <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
+              <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
+              <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                <span className='text-white text-sm'>{technology.name}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <p className='text-description md:text-[30px] text-[15px] p-4'>TOOLS</p>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {tools.map((technology) => (
-          <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
-            <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
-            <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-              <span className='text-white text-sm'>{technology.name}</span>
+
+      <div className='flex flex-col flex-1'>
+        <p className='text-title md:text-[30px] text-[24px] text-center p-4'>TOOLS</p>
+        <div className='flex flex-row flex-wrap gap-5 justify-center'>
+          {tools.map((technology) => (
+            <div className='group w-28 h-28 p-4 hover:border hover:cursor-pointer hover:scale-110 rounded-2xl bg-[#202329] overflow-hidden relative' key={technology.name}>
+              <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
+              <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                <span className='text-white text-sm'>{technology.name}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </motion.div>
   );
